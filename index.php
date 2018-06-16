afdeaff6203c54c5145143e9c31da3a1881cf768
Alfred Quiambao <alfredquiambaoiii@gmail.com>
2018-06-16 23:38:13 +0800

    Added Login form

--- index.php
+++ index.php
@@ -2,8 +2,42 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/css/uikit.min.css" />
+
 </head>
 <body>
-<?php echo "HELLO WORLD!"; ?>
+
+
+<div class=".uk-section.uk-container uk-text-center">
+	<div class="uk-grid uk-child-width"></div>
+
+<form class="js-login">
+
+    <div class="uk-margin">
+        <div class="uk-inline">
+            <span class="uk-form-icon" uk-icon="icon: user"></span>
+            <input placeholder="Username" class="uk-input" type="email">
+        </div>
+    </div>
+
+    <div class="uk-margin">
+        <div class="uk-inline">
+            <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+            <input placeholder="Password" class="uk-input" type="password">
+        </div>
+    </div>
+
+    <div class="uk-margin">
+        <button class="uk-button uk-button-default">Login </button>
+    </div>
+
+</form>
+
+</div>
+<!-- jQuery is required -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/js/uikit.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.6/js/uikit-icons.min.js"></script>
 </body>
 </html>
